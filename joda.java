eb2627a65ab89c1f4e81f48dea6181d07e7dcd85
AnUnknownC <88901481+AnUnknownC@users.noreply.github.com>
2024-08-21 18:04:11 -0500

    Add files via upload

---
+++ joda.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String palabra1 = "Hello";
+        String palabra2 = " World";
+        System.out.println(palabra1+palabra2);
+    }
+}
