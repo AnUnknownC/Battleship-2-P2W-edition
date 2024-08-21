41a72f13c2cfbe7e2c6a518aa13511250816a2fd
JDiazRzo <159645527+JDiazRzo@users.noreply.github.com>
2024-08-21 18:33:16 -0500

    Add files via upload

--- joda.java
+++ joda.java
@@ -1,9 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class joda {
     public static void main(String[] args) {
-        String palabra1 = "Hello";
-        String palabra2 = " World";
-        System.out.println(palabra1+palabra2);
+        int numero = 2;
+        System.out.println("Hola mundo " + numero);
     }
 }
