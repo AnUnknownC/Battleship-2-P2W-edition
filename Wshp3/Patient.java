b2ae676f57084ecca953b2db7b6325fe7cc6f5c2
JDiazRzo <159645527+JDiazRzo@users.noreply.github.com>
2024-08-22 19:43:18 -0500

    Add files via upload

---
+++ Wshp3/Patient.java
@@ -0,0 +1,21 @@
+class Patient {
+
+    int age;
+    String name;
+    String condition;
+    String id;
+    String diagnostic;
+    String lifeExpectancy;
+
+    public void Patient(String name, String condition, String id, String diagnostic, String lifeExpectancy, int age) {
+        this.age = age;
+        this.name = name;
+        this.condition = condition;
+        this.id = id;
+        this.diagnostic = diagnostic;
+        this.lifeExpectancy = lifeExpectancy;
+
+    }
+
+}
+
