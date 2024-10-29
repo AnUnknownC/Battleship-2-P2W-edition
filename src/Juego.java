884df99cfe518a6b4c84f7cce90c322a40cb316a
JDiazRzo <159645527+JDiazRzo@users.noreply.github.com>
2024-10-29 10:27:00 -0500

    Add files via upload

---
+++ src/Juego.java
@@ -0,0 +1,11 @@
+public class Juego {
+    
+    private Jugador jugador1;
+    private Jugador jugador2;
+    private Tablero tablero1;
+    private Tablero tablero2;
+    private Tienda tienda;
+
+
+
+}
